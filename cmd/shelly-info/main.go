52e962fa8016d288f3a5fb8f423476cfaccb02ee
Juan Cespedes <cespedes@thehackers.org>
2021-10-17 13:10:00 +0200

    shelly-info is working

--- cmd/shelly-info/main.go
+++ cmd/shelly-info/main.go
@@ -20,5 +20,50 @@ func main() {
 		os.Exit(2)
 	}
 
-	fmt.Printf("SHELLY: %+v\n", shelly)
+	fmt.Printf("Type: %s\n", shelly.Settings.Device.Type)
+	fmt.Printf("MAC: %s\n", shelly.Settings.Device.MAC)
+	if shelly.Settings.Mode != "" {
+		fmt.Printf("Mode: %s\n", shelly.Settings.Mode)
+	}
+	fmt.Printf("SSID: %s\n", shelly.Status.WiFiStatus.SSID)
+	fmt.Printf("IP: %s\n", shelly.Status.WiFiStatus.IP)
+	fmt.Printf("Signal: %d\n", shelly.Status.WiFiStatus.RSSI)
+	if shelly.Status.Temperature != 0.0 {
+		fmt.Printf("Temperature: %.02f C\n", shelly.Status.Temperature)
+	}
+	if shelly.Status.Voltage != 0.0 {
+		fmt.Printf("Voltage: %.02f V\n", shelly.Status.Voltage)
+	}
+	for i, v := range shelly.Status.Inputs {
+		fmt.Printf("Input %d:\n", i)
+		fmt.Printf("\tInput: %d\n", v.Input)
+	}
+	for i, v := range shelly.Status.Relays {
+		fmt.Printf("Relay %d:\n", i)
+		fmt.Printf("\tIsOn: %v\n", v.IsOn)
+		fmt.Printf("\tSource: %s\n", v.Source)
+		if len(shelly.Status.Meters) > i {
+			fmt.Printf("\tPower: %.02f W\n", shelly.Status.Meters[i].Power)
+			fmt.Printf("\tTotal: %d Wh\n", shelly.Status.Meters[i].Total)
+		}
+	}
+	for i, v := range shelly.Status.Rollers {
+		fmt.Printf("Roller %d:\n", i)
+		fmt.Printf("\tState: %v\n", v.State)
+		if v.Positioning {
+			fmt.Printf("\tPos: %d%%\n", v.CurrentPos)
+		}
+		if len(shelly.Status.Meters) > 2*i+1 {
+			fmt.Printf("\tPower: %.02f W\n", shelly.Status.Meters[2*i].Power+shelly.Status.Meters[2*i+1].Power)
+			fmt.Printf("\tTotal: %d Wh\n", shelly.Status.Meters[2*i].Total+shelly.Status.Meters[2*i+1].Total)
+		}
+	}
+	for i, v := range shelly.Status.Emeters {
+		fmt.Printf("Emeter %d:\n", i)
+		fmt.Printf("\tVoltage: %.02f V\n", v.Voltage)
+		fmt.Printf("\tPower: %.02f W\n", v.Power)
+		fmt.Printf("\tTotal: %.02f Wh\n", v.Total)
+		fmt.Printf("\tTotalReturned: %.02f Wh\n", v.TotalReturned)
+	}
+	// fmt.Printf("SHELLY: %+v\n", shelly)
 }
